70c32bc5eca8685d9169d58c531ce8a374ef6205
The-raven-god <161849446+The-raven-god@users.noreply.github.com>
2025-07-17 21:41:37 -0500

    Postgresql

--- SystemPoa/settings.py
+++ SystemPoa/settings.py
@@ -73,12 +73,15 @@ WSGI_APPLICATION = 'SystemPoa.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "SystemPOA",
+        "USER": "est_s_rueda",
+        "PASSWORD": "5E3c45k0TEhi5ttC",
+        "HOST": "postgres.juansegaliz.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
